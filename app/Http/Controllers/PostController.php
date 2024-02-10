4ae45cdd7df93d8cfbece1c560f04b4aa1fb2261
KaungZayY <kaungzayyan19@gmail.com>
2024-02-10 13:59:47 +0630

    Post Create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function create(){
+        return view('posts.post-create');
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+            'title'=>'required|string',
+            'body'=>'required|string'
+        ]);
+
+        $posted=$request->user()->posts()->create([
+            'title'=>$request->title,
+            'body'=>$request->body
+        ]);
+        if (!$posted) {
+            return back()->with('error', 'Cannot Upload at the moment');
+        } else {
+            return redirect()->route('dashboard')->with('success', 'Upload successful');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Posts
+    Route::get('/post/create',[PostController::class, 'create'])->name('post.create');
+    Route::post('/post/create',[PostController::class, 'store']);
 });
 
 require __DIR__.'/auth.php';
